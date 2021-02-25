1bc400b229d375c6c1d99b3171bac0ffe84b87ee
Валерий Кулаков <kulakov.angel@gmail.com>
2021-02-25 10:55:50 +0300

    Init Commit

---
+++ netlify/functions/hello.js
@@ -0,0 +1,21 @@
+const querystring = require('querystring');
+const fetch = require('node-fetch');
+
+// const API_ENDPOINT = "https://www.nbrb.by/api/exrates/currencies/145";
+const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
+
+const handler = async (event, context) => {
+
+    const {userId} = querystring.parse(event.body);
+
+    const a = await fetch(API_ENDPOINT)
+        .then(response => response.json())
+        .then(data => data[0])
+        .catch((error) => ({ statusCode: 422, body: String(error) }));
+    return ({
+        statusCode: 200,
+        body: a,
+    });
+};
+
+module.exports = handler;
